9a6c1a043625df68d7c4edc517fc0a218381da2d
David Yoon <dyoon0807@gmail.com>
2023-06-03 23:33:05 +0900

    App created

--- ai_image_generator/ai_image_generator/settings.py
+++ ai_image_generator/ai_image_generator/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chatgpt_connector',
 ]
 
 MIDDLEWARE = [
---
+++ ai_image_generator/chatgpt_connector/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChatgptConnectorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chatgpt_connector'
