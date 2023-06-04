61519243dc53796ad91b8e6dab2974d725371fbf
David Yoon <dyoon0807@gmail.com>
2023-06-04 11:32:59 +0900

    View created

--- ai_image_generator/chatgpt_connector/models.py
+++ ai_image_generator/chatgpt_connector/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    prompt = models.CharField(max_length=200)
+    description = models.TextField()
     size = models.CharField(max_length=10)
+    url = models.CharField(max_length=200)
--- ai_image_generator/chatgpt_connector/views.py
+++ ai_image_generator/chatgpt_connector/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from chatgpt_connector.forms import *
+
 
 # Create your views here.
+def index(request):
+    form = Form()
+    return render(request, 'chatgpt_connector/index.html', {'form': form})
